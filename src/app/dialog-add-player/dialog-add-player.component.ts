cf8bc6119e80f43b39b0ce2a57af9e390556d23f
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-01-15 13:53:45 +0100

    dialog

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -31,7 +31,7 @@ import { MatInputModule } from '@angular/material/input';
 })
 
 
-export class DialogAddPlayerComponent implements OnInit {
+export class DialogAddPlayerComponent implements OnInit  {
   name: string = '';
 
   constructor() { }
